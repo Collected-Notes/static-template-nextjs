185fb5d7400172993156057654857cbf740dc22a
Sergio Xalambrí <hello@sergiodxa.com>
2020-08-15 05:31:08 -0500

    Create NoteItem component

---
+++ src/components/note-item.tsx
@@ -0,0 +1,16 @@
+import { Note } from "collected-notes";
+import Link from "next/link";
+import styles from "components/note-item.module.css"
+
+export function NoteItem({ note }: { note: Note }) {
+  return (
+    <Link href="[path]" as={`/${note.path}`}>
+      <a>
+        <article className={styles.note}>
+          <h2>{note.title}</h2>
+          <p>{note.headline}</p>
+        </article>
+      </a>
+    </Link>
+  );
+}
