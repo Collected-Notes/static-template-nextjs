0929ed18cc5b81c804d1f2757d5966e121ef537e
Sergio Xalambrí <hello@sergiodxa.com>
2020-08-15 05:30:35 -0500

    Create types file with different page types

---
+++ src/types.d.ts
@@ -0,0 +1,14 @@
+import { Note, Site, Link, HTML } from "collected-notes";
+
+export type ArticlePageProps = {
+  note: Note;
+  site: Site;
+  body: HTML;
+  links: Link[];
+};
+
+export type ArticlePageQuery = { path: string };
+
+export type HomePageProps = { site: Site; notes: Note[] };
+
+export type SearchPageProps = { site: Site; }
