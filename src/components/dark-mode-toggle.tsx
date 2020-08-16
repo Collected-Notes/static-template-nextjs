f723fdb107a24427aea7d60f369d1a5c35e12422
Sergio Xalambrí <hello@sergiodxa.com>
2020-08-15 23:06:02 -0500

    Improve styles

--- src/components/dark-mode-toggle.tsx
+++ src/components/dark-mode-toggle.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import useDarkMode from "use-dark-mode";
 import { FaSun, FaMoon } from "react-icons/fa";
+import styles from "components/dark-mode-toggle.module.css";
 
 export function DarkModeToggle() {
   const { toggle, value } = useDarkMode();
@@ -8,19 +9,11 @@ export function DarkModeToggle() {
   const label = value ? "Disable dark mode" : "Enable dark mode";
 
   return (
-    <button
-      aria-label={label}
-      onClick={toggle}
-      className="bg-black p-2 rounded-full flex items-center space-x-3 relative"
-    >
+    <button aria-label={label} onClick={toggle} className={styles.button}>
       <FaSun color="#F6E05E" />
       <FaMoon color="#F6E05E" />
       <span
-        className="absolute w-8 h-8 bg-white rounded-full transition-all duration-200 left-0 top- bottom-0 border border-black"
-        style={{
-          marginLeft: 0,
-          transform: value ? "translateX(0px)" : "translateX(28px)",
-        }}
+        style={{ transform: value ? "translateX(0px)" : "translateX(28px)" }}
       />
     </button>
   );
